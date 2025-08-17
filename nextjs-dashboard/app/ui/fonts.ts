53be032f61586cf2e97cf252b01039a1028a3122
Suresh Kumar <sureshkumar79061@gmail.com>
2025-08-17 16:48:42 +0530

    Initial commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+import { Lusitana } from 'next/font/google';
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400', // ya '700' depending on need
+});
